647db24d148889177b5c4df7409a0ddd46c09bd6
Ross McKay <rmckay@webaware.com.au>
2016-07-02 19:26:01 +1000

    remove .pot file, no longer required with translate.w.org

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,42 +59,6 @@ module.exports = function (grunt) {
 			dist: {
 				src: "css/*.css"
 			}
-		},
-
-		pot: {
-			options: {
-				text_domain: "log-emails",
-				msgid_bugs_address: "translate@webaware.com.au",
-				encoding: "UTF-8",
-				dest: "languages/",
-				keywords: [
-					"gettext",
-					"__",
-					"_e",
-					"_n:1,2",
-					"_x:1,2c",
-					"_ex:1,2c",
-					"_nx:4c,1,2",
-					"esc_attr__",
-					"esc_attr_e",
-					"esc_attr_x:1,2c",
-					"esc_html__",
-					"esc_html_e",
-					"esc_html_x:1,2c",
-					"_n_noop:1,2",
-					"_nx_noop:3c,1,2",
-					"__ngettext_noop:1,2"
-				],
-				comment_tag: "translators:"
-			},
-			files: {
-				src: [
-					"**/*.php",
-					"!lib/**/*",
-					"!node_modules/**/*"
-				],
-				expand: true
-			}
 		}
 
 	});
@@ -102,7 +66,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-sass");
 	grunt.loadNpmTasks("grunt-postcss");
-	grunt.loadNpmTasks("grunt-pot");
 
 	grunt.registerTask("scss", ["sass","postcss"]);
 	grunt.registerTask("default", [ "jshint" ]);
