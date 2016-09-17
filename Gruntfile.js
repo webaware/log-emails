89a9a6e281eaefc9dad579a17a13f3503e04da78
Ross McKay <rmckay@webaware.com.au>
2016-09-17 17:57:41 +1000

    grunt config for building release .zip

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,33 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
 
+		clean: [ "dist/**" ],
+
+		copy: {
+			main: {
+				files: [
+					{
+						src: ["./**", "!./node_modules/**", "!./Gruntfile.js", "!./package.json"],
+						dest: "dist/<%= pkg.name %>/"
+					}
+				]
+			}
+		},
+
+		compress: {
+			options: {
+				archive: "./dist/<%= pkg.name %>-<%= pkg.version %>.zip",
+				mode: "zip"
+			},
+			all: {
+				files: [{
+					expand: true,
+					cwd: "./dist/",
+					src: [ "<%= pkg.name %>/**" ]
+				}]
+			}
+		},
+
 		jshint: {
 			all: [
 				"Gruntfile.js",
@@ -63,10 +90,14 @@ module.exports = function (grunt) {
 
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks("grunt-contrib-compress");
+	grunt.loadNpmTasks("grunt-contrib-copy");
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-sass");
 	grunt.loadNpmTasks("grunt-postcss");
 
+	grunt.registerTask("release", ["clean","copy","compress"]);
 	grunt.registerTask("scss", ["sass","postcss"]);
 	grunt.registerTask("default", [ "jshint" ]);
 
